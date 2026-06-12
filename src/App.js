c6fd4279effb7c44473d8ea3974216cb4c282027
Harpreet <ksfibri@gmail.com>
2026-06-12 14:33:10 -0500

    Add Image

--- src/App.js
+++ src/App.js
@@ -4,37 +4,36 @@ import TaskList from "./components/TaskList";
 import { taskReducer } from "./reducer/taskReducer";
 
 function App() {
-    const [tasks, dispatch] = useReducer(taskReducer, []);
-    const [text, setText] = useState("");
-    const [editId, setEditId] = useState(null);
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+  const [text, setText] = useState("");
+  const [editId, setEditId] = useState(null);
 
-    return (
-        <div className="app">
-            <div className="app-header">
-                <div className="logo"></div>
-
-                <div>
-                    <h1>My Tasks</h1>
-                    <p className="count">{tasks.length} tasks</p>
-                </div>
-            </div>
-
-            <TaskForm
-                text={text}
-                setText={setText}
-                editId={editId}
-                setEditId={setEditId}
-                dispatch={dispatch}
-            />
-
-            <TaskList
-                tasks={tasks}
-                dispatch={dispatch}
-                setText={setText}
-                setEditId={setEditId}
-            />
+  return (
+    <div className="app">
+      <div className="app-header">
+<img src={`${process.env.PUBLIC_URL}/image.png`} alt="logo" className="logo" />
+        <div>
+          <h1>My Tasks</h1>
+          <p className="count">{tasks.length} tasks</p>
         </div>
-    );
+      </div>
+
+      <TaskForm
+        text={text}
+        setText={setText}
+        editId={editId}
+        setEditId={setEditId}
+        dispatch={dispatch}
+      />
+
+      <TaskList
+        tasks={tasks}
+        dispatch={dispatch}
+        setText={setText}
+        setEditId={setEditId}
+      />
+    </div>
+  );
 }
 
 export default App;
